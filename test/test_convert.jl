07012c3e063c0d95ed0acd212b84029085cec9aa
hayesall <alexander@batflyer.net>
2021-12-05 08:05:06 -0500

    ✅ Add tests for multiclass case

--- test/test_convert.jl
+++ test/test_convert.jl
@@ -101,6 +101,32 @@ using RelationalDatasets
             "c(+id).",
         ]
     )
+
+    @test RelationalDatasets.from_vector(
+        [0 1 1; 1 0 2; 2 2 0],
+        [0, 1, 2],
+    ) == (RelationalDatasets.RelationalDataset((
+            pos=["v4(id1,0).", "v4(id2,1).", "v4(id3,2)."],
+            neg=[],
+            facts=[
+                "v1(id1,0).",
+                "v1(id2,1).",
+                "v1(id3,2).",
+                "v2(id1,1).",
+                "v2(id2,0).",
+                "v2(id3,2).",
+                "v3(id1,1).",
+                "v3(id2,2).",
+                "v3(id3,0).",
+            ])),
+        [
+            "v1(+id,#varv1).",
+            "v2(+id,#varv2).",
+            "v3(+id,#varv3).",
+            "v4(+id,#classlabel).",
+        ]
+    )
+
 end
 
 @testset "Misaligned Classification and Regression" begin
